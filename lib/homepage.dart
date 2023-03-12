69a5b7c05a4d50dd9a54442a560beed662cb946a
@Bahatimyk <manenomichael99@gmail.com>
2023-03-12 15:11:45 +0300

    linking the pages

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projectsample/more.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -7,7 +8,15 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return const More();
+              },
+            ),
+          );
+        },
         child: const Text('Learn more'),
       ),
     );
